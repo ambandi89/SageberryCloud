e0e8a2083dfa540bd4aea3d8fb8dcc57d8275657
ambandi89 <ambandi89@gmail.com>
2026-03-03 12:10:21 -0600

    Add files via upload

--- facts.js
+++ facts.js
@@ -8,3 +8,24 @@
     document.addEventListener('click', function() {
         tooltipWrapper.classList.remove('tooltip-active');
     });
+
+    // scrolling
+
+function checkNerdyBitsVisibility() {
+    const scrolled = window.scrollY / (document.documentElement.scrollHeight - window.innerHeight);
+
+    const nerdybits1 = document.querySelector('.nerdybits1');
+    const nerdybits2 = document.querySelector('.nerdybits2');
+    const rect1 = nerdybits1.getBoundingClientRect();
+
+    if (rect1.top < window.innerHeight * 0.50) {
+        nerdybits1.classList.add('nerdy-visible');
+    }
+
+    if (scrolled >= 0.30) {
+        nerdybits2.classList.add('nerdy-visible');
+    }
+}
+
+window.addEventListener('scroll', checkNerdyBitsVisibility);
+checkNerdyBitsVisibility();
